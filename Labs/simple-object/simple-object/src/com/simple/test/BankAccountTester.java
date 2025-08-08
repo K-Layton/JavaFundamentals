fb07130457f10ff9f5a70973624e3a09fee8afb3
K-Layton <140290262+K-Layton@users.noreply.github.com>
2025-08-08 09:29:43 -0400

    Add files via upload

---
+++ Labs/simple-object/simple-object/src/com/simple/test/BankAccountTester.java
@@ -0,0 +1,39 @@
+package com.simple.test;
+
+import com.simple.account.BankAccount;
+import com.simple.account.SavingsAccount;
+
+public class BankAccountTester {
+
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount(1, "Jeff Lebowski", 100f);
+        System.out.println("A Bank Account");
+        System.out.println("ID: " + account.getAccountID());
+        System.out.println("Balance: " + account.getBalance());
+        System.out.println("Owner: " + account.getOwnerName());
+
+        BankAccount account2 = new BankAccount(2, "Bunny Lebowski", 5000f);
+        BankAccount account3 = new BankAccount(3, "Walter Sobcheck", 1000000f);
+
+        System.out.println("account2 is owned by " + account2.getOwnerName());
+        BankAccount.setInterestRate(5.5f);
+
+        System.out.println("account3 is owned by " + account3.getOwnerName());
+        account2.deposit(100f);
+        account3.deposit(900f);
+        System.out.println("account2's balance is " + account2.getBalance());
+        System.out.println("account3's balance is " + account3.getBalance());
+        System.out.println("account2's interestRate is " + BankAccount.getInterestRate());
+        System.out.println("account3's interestRate is " + BankAccount.getInterestRate());
+
+        SavingsAccount sAccount = new SavingsAccount(3, "Donny K", 1f);
+        System.out.println("The SavingAccount's balance is " + sAccount.getBalance());
+
+        sAccount.deposit(500f);
+        sAccount.payInterest();
+        System.out.println("The SavingAccount's new balance is " + sAccount.getBalance());
+
+        
+    }
+
+}
