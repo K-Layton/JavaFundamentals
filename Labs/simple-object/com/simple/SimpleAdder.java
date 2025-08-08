003e98014427b8f7e9d016f64d78abcfbeb5e4c9
K-Layton <Kevin.r.macias16@gmail.com>
2025-08-08 10:14:46 -0400

    added exception SimpleAdder

---
+++ Labs/simple-object/com/simple/SimpleAdder.java
@@ -0,0 +1,40 @@
+package com.simple;
+
+import java.util.Scanner;
+
+public class SimpleAdder {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		Scanner scan = new Scanner(System.in);
+		int numbers[] = new int[10];
+
+		for (int x = 0; x < 10; x++) {
+			System.out.println("Please enter integer #" + (x + 1));
+			// int input = scan.nextInt();
+			int input = 0;
+			try {
+				input = scan.nextInt();
+			} catch (java.util.InputMismatchException exception) {
+				System.out.println("Please enter only numeric values.");
+				break;
+			}
+
+			if (input < 0) {
+				continue;
+			}
+			if (input == 0) {
+				break;
+			}
+			sum += input;
+			numbers[x] = input;
+		}
+		System.out.println("The numbers you entered were: ");
+		for (int eachNumber : numbers) {
+			System.out.println(eachNumber);
+		}
+		System.out.println("The sum is " + sum);
+		scan.close();
+	}
+
+}
